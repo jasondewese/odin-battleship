723ce6a8ae679f01e7c606bc8f704a143982cc9c
Jason Dewese <19419757+jasondewese@users.noreply.github.com>
2022-04-12 21:04:46 -0500

    Add Gameboard.receiveAttack and unit test

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,15 +1,18 @@
 import { Ship } from "./Ship";
 
 const Gameboard = () => {
-
+    const _BOARDSIZE = 10;
     let _board = [];
     let _shipList = [];
+    let _shotsFired = [];
     
     const _initBoard = (() => {
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < _BOARDSIZE; i++) {
             _board.push([]);
-            for (let j = 0; j < 10; j++) {
+            _shotsFired.push([]);
+            for (let j = 0; j < _BOARDSIZE; j++) {
                 _board[i].push(-1);
+                _shotsFired[i].push(-1);
             }
         }
     })();
@@ -20,29 +23,48 @@ const Gameboard = () => {
         }
     })();
 
-    const placeShip = (shipIndex, x1,y1,x2,y2) => {
+    const placeShip = (ship, x1,y1,x2,y2) => {
+
+        let currIndex = 0;
 
         for (let i = x1; i <= x2; i++) {
             for (let j = y1; j <= y2; j++) {
-                _board[i][j] = shipIndex;
+                _board[i][j] = {
+                    ship: ship,
+                    shipIndex: currIndex
+                };
+                currIndex++;
             }
         }
 
-        
-        if (shipIndex >= _shipList.length || shipIndex < 0) {
+        if (ship >= _shipList.length || ship < 0) {
             throw new Error('No ship exists at provided index');
         }
-        if (x1 >= 10 || y1 >= 10 ||x2 >= 10 || y2 >= 10) {
+        if (x1 >= _BOARDSIZE || y1 >= _BOARDSIZE ||x2 >= _BOARDSIZE || y2 >= _BOARDSIZE) {
             throw new Error('Invalid coordinates received. Out of bounds');
         }
         if (x1 < 0 || y1 < 0|| x2 < 0 || y2 < 0) {
             throw new Error('Invalid coordinates received. Out of bounds');
         }
         
-        return [_board[x1][y1], _board[x2][y2]];
+        return [_board[x1][y1].ship, _board[x2][y2].ship];
     }
 
-    return {placeShip};
+    const receiveAttack = (x1,y1) => {
+        const MISS = 0;
+        const HIT = 1;
+        const boardValue = typeof _board[x1][y1] === 'object' ? _board[x1][y1].ship : _board[x1][y1];
+
+        if (boardValue !== -1) {
+            _shipList[boardValue].hit(_board[x1][y1].shipIndex);
+            return 'HIT';
+        }
+        else {
+            return 'MISS';
+        }
+    }
+
+    return {placeShip, receiveAttack};
 }
 
 export {Gameboard};
--- src/Gameboard.test.js
+++ src/Gameboard.test.js
@@ -12,3 +12,21 @@ test('placeShip returns error for invalid ship or coordinates', () => {
     expect(() => {gameboard.placeShip(1,12,2,4,2)}).toThrow('Invalid coordinates received. Out of bounds');
 
 });
+
+test('receiveAttack determines hit or miss properly', () => {
+    let gameboard = Gameboard();
+    gameboard.placeShip(0,0,0,1,1);
+    gameboard.placeShip(2,2,2,4,2);
+
+    expect(gameboard.receiveAttack(0,0)).toBe('HIT');
+    expect(gameboard.receiveAttack(7,7)).toBe('MISS');
+});
+
+test('receiveAttack calls Ship.hit on appropriate ship if a hit occurs', () => {
+    let gameboard = Gameboard();
+    gameboard.placeShip(0,0,0,1,1);
+    gameboard.placeShip(2,2,2,4,2);
+    expect(gameboard.receiveAttack(0,0)).toBe('HIT');
+    expect(gameboard.receiveAttack(0,5)).toBe('MISS');
+
+});
